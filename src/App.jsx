024c1823824eab42ea4eaef448585d14e97f379d
Amrita Mathur <amrita.mathur@matellio.com>
2025-07-28 10:57:28 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize('G-CPNSL6DNTK');
 
 function App() {
+  
   const [count, setCount] = useState(0)
+
+  useEffect(()=> {
+    ReactGA.initialize('G-CPNSL6DNTK');
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "App.jsx" });
+
+  }, [])
 
   return (
     <>
